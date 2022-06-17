a01adf329b59b5cf2df1a92be09bc8e754bc2c9e
vitalikod <vitalikod879@gmail.com>
2022-06-17 19:42:04 +0300

    dddd

---
+++ script/main.js
@@ -0,0 +1,40 @@
+let a = '';
+let b = '';
+let sign = '';
+let finish = false;
+
+const digit = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9',];
+const action = ['-', '+', '/', '*',];
+
+const out = document.querySelector('.screen');
+
+function clearAll () {
+    a = '';
+    b = '';
+    sign = '';
+    finish = false;
+    out.textContent = 0;
+}
+
+document.querySelector('.ac').onclick = clearAll;
+
+document.querySelector('.buttons').onclick = (event) => {
+    if(!event.target.classList('button')) return;
+    if(event.target.classList('ac')) return;
+
+    out.textContent = '';
+
+    const key = event.target.textContent;
+
+    if (digit.includes(key)) {
+        a += key;
+        console.log(a, b , sign);
+        out.textContent = a;
+    }
+    if (action.includes(key)) {
+        sign = key;
+        out.textContent = sign;
+        console.log(a, b , sign);
+        return;
+    }
+}
